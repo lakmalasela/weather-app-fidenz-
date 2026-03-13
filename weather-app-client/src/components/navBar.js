b53d3df74a4bbda4bbc6898814b616b81fb11bd6
lakmalasela <lakmaljs160@gmail.com>
2026-03-13 12:43:25 +0530

    icons added

--- weather-app-client/src/components/navBar.js
+++ weather-app-client/src/components/navBar.js
@@ -17,11 +17,10 @@ const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
-                <button className="navbar-brand" style={{background: 'none', border: 'none', color: 'white', fontSize: '1.25rem', padding: '0', cursor: 'pointer'}}>Weather App</button>
+                <button className="navbar-brand" style={{background: 'none', border: 'none', color: 'white', fontSize: '1.25rem', padding: '0', cursor: 'pointer'}}><i class="ri-heavy-showers-fill"></i> Weather App</button>
                 <div className="collapse navbar-collapse" id="navbarText">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            {/* <button className="nav-link active" style={{background: 'none', border: 'none', color: 'white', padding: '0.5rem 1rem', cursor: 'pointer'}} onClick={handleLogout}>Log Out</button> */}
                         </li>
                     </ul>
                     <span className="navbar-text d-flex align-items-center gap-2">
@@ -34,7 +33,7 @@ const NavBar = () => {
                             <i className={`ri-${isDarkMode ? 'sun' : 'moon'}-line`}></i>
                             {isDarkMode ? 'Light' : 'Dark'}
                         </button>
-                        <button className="nav-link active" style={{background: 'none', border: 'none', color: 'white', padding: '0.5rem 1rem', cursor: 'pointer'}} onClick={handleLogout}>Log Out</button>
+                        <button className="nav-link active" style={{background: 'none', border: 'none', color: 'white', padding: '0.5rem 1rem', cursor: 'pointer'}} onClick={handleLogout}><i class="ri-logout-box-fill"></i> Log Out</button>
                     </span>
                 </div>
             </div>
